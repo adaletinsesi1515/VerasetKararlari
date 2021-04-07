56fb6e52b0387f1ce080c4b2a6f4b44406014b5e
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2021-04-07 17:08:56 +0300

    Add project files.

---
+++ VerasetKararlari/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using VerasetKararlari.Models.Entity;
+
+namespace VerasetKararlari.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        private VerasetEntities db = new VerasetEntities();
+
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GirisYap(Tbl_Kullanicilar p)
+        {
+            var bilgiler =
+                db.Tbl_Kullanicilar.FirstOrDefault(x => x.SICIL == p.SICIL && x.SIFRE == p.SIFRE);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.SICIL, false);
+                Session["SICIL"] = bilgiler.SICIL.ToString();
+
+                return RedirectToAction("Index", "Mahkeme");
+            }
+            else
+            {
+                return View();
+            }
+
+
+
+        }
+        public ActionResult CikisYap()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("GirisYap", "Login");
+        }
+    }
+}
---
+++ VerasetKararlari/Controllers/MahkemeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VerasetKararlari.Models.Entity;
+
+namespace VerasetKararlari.Controllers
+{
+    public class MahkemeController : Controller
+    {
+        // GET: Mahkeme
+        private VerasetEntities db = new VerasetEntities();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var liste = db.Tbl_Veraset.Where(i => i.DURUM == true).ToList();
+            return View(liste);
+        }
+
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ekle(Tbl_Veraset p)
+        {
+            db.Tbl_Veraset.Add(p);
+            p.DURUM = true;
+            p.KAYITTARIHI = DateTime.Now;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Getir(int id)
+        {
+            var bilgi = db.Tbl_Veraset.Find(id);
+            return View("Getir", bilgi);
+        }
+
+        public ActionResult Guncelle(Tbl_Veraset p)
+        {
+            var bilgi = db.Tbl_Veraset.Find(p.ID);
+            bilgi.KARARNO= p.KARARNO;
+            bilgi.DAVACI= p.DAVACI;
+            bilgi.VEFATEDEN= p.VEFATEDEN;
+            bilgi.DURUM = true;
+            bilgi.KAYITTARIHI = DateTime.Now;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
